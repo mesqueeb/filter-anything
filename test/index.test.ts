ba182914dd00949bbd6570dc2c305ac6516e23ce
Luca Ban <mesqueeb@users.noreply.github.com>
2022-01-25 11:36:08 +0900

    build: cleanup 🧼

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,26 +1,26 @@
 import { pick, omit } from '../src/index'
 import pathsAreEqual from '../src/pathsAreEqual'
-import test from 'ava'
+import { test, expect } from 'vitest'
 
-test('check pick FLAT', (t) => {
+test('check pick FLAT', () => {
   const doc = { name: 'n1', id: '1', filled: true, notfilled: false }
   const res = pick(doc, ['name', 'filled', 'id'])
   t.deepEqual(res, { name: 'n1', id: '1', filled: true })
 })
 
-test('check pick 0 props', (t) => {
+test('check pick 0 props', () => {
   const doc = { name: 'n1', id: '1', filled: true, notfilled: false }
   const res = pick(doc, [] as any)
   t.deepEqual(res, {} as any)
 })
 
-test('check omit FLAT', (t) => {
+test('check omit FLAT', () => {
   const doc = { name: 'n1', id: '1', filled: true, omited: true }
   const res = omit(doc, ['omited'])
   t.deepEqual(res, { name: 'n1', id: '1', filled: true })
 })
 
-test('check picks NESTED - single pick', (t) => {
+test('check picks NESTED - single pick', () => {
   const doc = {
     nested: { picks: { yes: 0, no: 0 } },
     secondProp: true,
@@ -29,7 +29,7 @@ test('check picks NESTED - single pick', (t) => {
   t.deepEqual(res, { nested: { picks: { yes: 0 } } })
 })
 
-test('check picks NESTED - single pick 2', (t) => {
+test('check picks NESTED - single pick 2', () => {
   const doc = {
     nested: { picks: { yes: 0, no: 0 } },
     secondProp: true,
@@ -38,7 +38,7 @@ test('check picks NESTED - single pick 2', (t) => {
   t.deepEqual(res, { nested: { picks: { yes: 0 } } })
 })
 
-test('check picks NESTED - single pick 3', (t) => {
+test('check picks NESTED - single pick 3', () => {
   const doc = {
     nested: { picks: { yes: 0, no: 0 } },
     secondProp: true,
@@ -47,7 +47,7 @@ test('check picks NESTED - single pick 3', (t) => {
   t.deepEqual(res, { nested: { picks: { yes: 0, no: 0 } } })
 })
 
-test('check picks NESTED - multiple pick', (t) => {
+test('check picks NESTED - multiple pick', () => {
   const doc = {
     nested: { picks: { yes: 0, no: 0 } },
     secondProp: { yes: true, no: false },
@@ -63,7 +63,7 @@ test('check picks NESTED - multiple pick', (t) => {
   t.deepEqual(res3, { nested: { picks: { yes: 0, no: 0 } }, secondProp: { yes: true } })
 })
 
-test('check omit NESTED', (t) => {
+test('check omit NESTED', () => {
   const doc = {
     nested: { omit: { yes: 0, no: 0 } },
     secondProp: true,
@@ -82,7 +82,7 @@ test('check omit NESTED', (t) => {
   t.deepEqual(res4, { nested: { omit: { no: 0 } } } as any)
 })
 
-test('check NESTED wildcards - pick', (t) => {
+test('check NESTED wildcards - pick', () => {
   const doc = {
     picks: {
       '123': { yes: true, no: false },
@@ -104,7 +104,7 @@ test('check NESTED wildcards - pick', (t) => {
     },
   })
 })
-test('check NESTED wildcards - omit', (t) => {
+test('check NESTED wildcards - omit', () => {
   const doc = {
     picks: { '456': { yes: true, no: false } },
     omited: { '123': { yes: true, no: false }, '456': { yes: true, no: false } },
@@ -116,7 +116,7 @@ test('check NESTED wildcards - omit', (t) => {
   } as any)
 })
 
-test('pathsAreEqual', (t) => {
-  t.is(pathsAreEqual('bob', '*'), true)
-  t.is(pathsAreEqual('bob.and.alice', 'bob.*.alice'), true)
+test('pathsAreEqual', () => {
+  expect(pathsAreEqual('bob').toEqual('*'), true)
+  expect(pathsAreEqual('bob.and.alice').toEqual('bob.*.alice'), true)
 })
--- test/readme.test.ts
+++ test/readme.test.ts
@@ -1,7 +1,7 @@
 import { pick, omit } from '../src/index'
-import test from 'ava'
+import { test, expect } from 'vitest'
 
-test('1', t => {
+test('1', () => {
   const squirtle = { id: '007', name: 'Squirtle', type: 'water' }
 
   const withoutId = pick(squirtle, ['name', 'type'])
@@ -9,7 +9,7 @@ test('1', t => {
   t.deepEqual(withoutId, { name: 'Squirtle', type: 'water' })
 })
 
-test('2', t => {
+test('2', () => {
   const squirtle = { id: '007', name: 'Squirtle', type: 'water' }
 
   const withoutId = omit(squirtle, ['id'])
