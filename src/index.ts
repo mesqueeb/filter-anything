10bc11e8a437306fde17ddf2b6b31a567e9aeb8d
Luca Ban <mesqueeb@users.noreply.github.com>
2026-09-26 10:42:00 +1200

    chore: prettier

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,15 @@ import { recursiveOmit } from './recursiveOmit.js'
 import { recursivePick } from './recursivePick.js'
 
 /**
- * pick returns a new object with only the props you pick
+ * Pick returns a new object with only the props you pick
  *
- * @export
  * @template T
  * @template K
- * @param {T} obj the target object to pick props from
- * @param {K[]} keys an array of prop names you want to keep - allows dot-notation for nested props, eg. `nested.prop` will keep just `{ nested: { prop: 1 } }`
- * @returns {O.Pick<T, K>} a new object with just the picked props
+ * @param {T} obj The target object to pick props from
+ * @param {K[]} keys An array of prop names you want to keep - allows dot-notation for nested props,
+ *   eg. `nested.prop` will keep just `{ nested: { prop: 1 } }`
+ * @returns {O.Pick<T, K>} A new object with just the picked props
+ * @export
  */
 export function pick<T extends { [key in string]: unknown }, K extends string>(
   obj: T,
@@ -25,14 +26,14 @@ export function pick<T extends { [key in string]: unknown }, K extends string>(
 export const fillable = pick
 
 /**
- * omit returns a new object without the props you omit
+ * Omit returns a new object without the props you omit
  *
- * @export
  * @template T
  * @template K
- * @param {T} obj the target object to omit props from
- * @param {K[]} keys the prop names you want to omit
- * @returns {O.Omit<T, K>} a new object without the omitted props
+ * @param {T} obj The target object to omit props from
+ * @param {K[]} keys The prop names you want to omit
+ * @returns {O.Omit<T, K>} A new object without the omitted props
+ * @export
  */
 export function omit<T extends { [key in string]: unknown }, K extends string>(
   obj: T,
